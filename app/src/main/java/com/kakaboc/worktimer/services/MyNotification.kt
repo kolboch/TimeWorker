eaffbe8102fc6f376e9d8cfd2f57e4a2cfa7ceea
Karol <kolboch@gmail.com>
2018-03-25 16:06:24 +0200

    notifications channel handling, icon fix

--- app/src/main/java/com/kakaboc/worktimer/services/MyNotification.kt
+++ app/src/main/java/com/kakaboc/worktimer/services/MyNotification.kt
@@ -1,9 +1,14 @@
 package com.kakaboc.worktimer.services
 
+import android.annotation.SuppressLint
 import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
 import android.os.Build
+import android.support.annotation.RequiresApi
 import android.support.v4.app.NotificationCompat
 import android.support.v4.app.NotificationCompat.PRIORITY_MAX
 import com.kakaboc.worktimer.R
@@ -21,16 +26,27 @@ class MyNotification {
             builder?.setContentText(contentText)
         }
 
+        @SuppressLint("NewApi")
         fun createWorkTimeNotification(context: Context,
                                        intentStart: PendingIntent,
                                        intentStop: PendingIntent,
                                        contentIntent: PendingIntent): Notification {
 
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channelName = context.getString(R.string.channel_name)
+                val channelDescription = context.getString(R.string.channel_description)
+                val importance = NotificationManager.IMPORTANCE_LOW
+                val channel = NotificationChannel(CHANNEL_ID, channelName, importance)
+                channel.description = channelDescription
+                channel.setShowBadge(false)
+                val notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(channel)
+            }
             if (builder != null) {
                 return builder!!.build()
             }
             initBuilder(context)
-            return builder!!.setSmallIcon(R.mipmap.ic_launcher_foreground)
+            return builder!!.setSmallIcon(R.drawable.ic_notification)
                     .setContentTitle(context.getString(R.string.app_name))
                     .setContentText(context.getString(R.string.current_working_time))
                     .setAutoCancel(true)
